55b1207396ce4348216e0e817d69c2982fbb5cb7
Alexandra Baivel <abaivel@outlook.fr>
2025-06-01 21:45:25 +0200

    fix bug

--- frontend/src/pages/ListQRCodes/index.jsx
+++ frontend/src/pages/ListQRCodes/index.jsx
@@ -81,7 +81,7 @@ useEffect(() => {
 
       <Dialog open={openQRCodeForm} onClose={handleCloseQRCodeForm}>
          <DialogContent style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <QRCodeForm qrcode= {{qrcodeId : 0, nom:"", lien:"", type:""}} setDialogOpen={setOpenQRCodeForm} setQrCode={null} refresh={()=>window.location.reload()}/>
+            <QRCodeForm qrcode= {{id : 0, nom:"", lien:"", type:""}} setDialogOpen={setOpenQRCodeForm} setQrCode={null} refresh={()=>window.location.reload()}/>
          </DialogContent>
       </Dialog>
     </div>
